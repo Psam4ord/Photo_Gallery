08c4e71d92df6fbd70f47e5c5f3bc6275f861bc4
Hayford Sammor-duah <psam4ord@gmail.com>
2022-03-28 10:25:48 +0100

    photo update001

---
+++ src/Components/Title.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Title = () =>{
+
+    return(
+        <div>
+        <h1>Fire_Gallery</h1>
+        <h1> Your Pictures</h1>
+        <p> One Stop Photo Gallery</p>
+        </div>
+    )
+
+   
+}
+export default Title
+
